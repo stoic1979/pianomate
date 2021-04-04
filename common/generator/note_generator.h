089117509c562098f20f2f046adebb0fe175edad
Navjot Singh <navisinghnz9@gmail.com>
2021-04-04 21:26:40 +1200

    added draft methods in note generator interface to generate and print notes

--- common/generator/note_generator.h
+++ common/generator/note_generator.h
@@ -1,6 +1,9 @@
 #ifndef __NOTE_GENERATOR_H__
 #define __NOTE_GENERATOR_H__
 
+#include <string>
+#include <vector>
+#include <map>
 
 struct Note {
   std::string name;
@@ -14,8 +17,19 @@ enum MusicNotation {
 
 class NoteGenerator {
 
-  virtual void GenerateRandomNotes(int count, bool repeat=true) = 0;
+  public:
+    static NoteGenerator* CreateNoteGenerator(MusicNotation musicNotation);
 
+    NoteGenerator(std::map <int, std::string> mapNotes): _mapNotes(mapNotes){}
+
+    virtual std::vector<Note> GenerateRandomNotes(int count, bool repeat=true);
+    virtual void PrintNotes(std::vector<Note> notes);
+    virtual void SerializeNotes(std::vector<Note> notes);
+
+    virtual MusicNotation GetMusicNotation() = 0;
+
+  protected:
+    std::map <int, std::string> _mapNotes;
 };
 
 #endif
